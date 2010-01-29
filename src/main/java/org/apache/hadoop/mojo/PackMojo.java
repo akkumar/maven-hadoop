d770621d23aecef8fcfb5548458095177049b26d
akkumar <akk.elance@gmail.com>
2010-01-29 14:12:46 -0800

    More javadocs README added.

--- src/main/java/org/apache/hadoop/mojo/PackMojo.java
+++ src/main/java/org/apache/hadoop/mojo/PackMojo.java
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2010 The Apache Software Foundation
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.mojo;
 
 import java.io.BufferedInputStream;
@@ -24,12 +43,43 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 
 /**
- * Deploy the hadoop class
+ * Pack the dependencies into an archive to be fed to the hadoop jar command.
+ * <p>
+ * Important: The list of dependencies in the hadoop CP are ignored / filtered
+ * to avoid namespace collision.
+ * <p>
+ * Internal Workings:
+ * <ul>
+ * <li>To go hand in hand with the compile target, this copies the dependencies
+ * to target/hadoop-deploy/lib directory.</li>
+ * <li>The dependencies that are already present in $HADOOP_HOME/lib/*.jar , if
+ * present in the project's dependencies are ignored.</li>
+ * </ul>
+ * <p>
+ * <h3>Installation</h3> Install the plugin as follows.
  * 
- * @goal deploy
+ * <pre>
+ * 
+ *  &lt;plugin&gt; 
+ *         &lt;groupId&gt;com.github.maven-hadoop.plugin&lt;/groupId&gt;
+ *         &lt;artifactId&gt;maven-hadoop-plugin&lt;/artifactId&gt;
+ *         &lt;version&gt;0.2.0&lt;/version&gt;
+ *         &lt;configuration&gt;
+ *           &lt;hadoopHome&gt;/opt/software/hadoop&lt;/hadoopHome&gt;
+ *         &lt;/configuration&gt;
+ *      &lt;/plugin&gt;
+ * </pre>
+ * 
+ * <h3>Usage:</h3>
+ * 
+ * <code>
+ * $ mvn compile hadoop:pack
+ * </code>
+ * 
+ * @goal pack
  * 
  */
-public class DeployMojo extends AbstractMojo {
+public class PackMojo extends AbstractMojo {
 
   /**
    * The maven project.
