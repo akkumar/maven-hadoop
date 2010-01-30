d86f64f71d34abb8c34b2ef4fe2850b44a25dff0
akkumar <akk.elance@gmail.com>
2010-01-29 17:22:34 -0800

    rename package as appropriate

--- src/main/java/com/github/hadoop/maven/plugin/PackMojo.java
+++ src/main/java/com/github/hadoop/maven/plugin/PackMojo.java
@@ -19,7 +19,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.hadoop.mojo;
+package com.github.hadoop.maven.plugin;
 
 import java.io.BufferedInputStream;
 import java.io.File;
